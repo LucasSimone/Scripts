bf5a6a8d75ff9e78e06cad27866475fd8d165a31
Lucas Simone <46714774+LucasSimone@users.noreply.github.com>
2024-02-28 14:09:00 -0500

    Updated to read from .txt. Updated print

--- python/https_checker/httpChecker.py
+++ python/https_checker/httpChecker.py
@@ -1,8 +1,13 @@
 import requests
+import sys
+
+
+
 
 # Gets a list of Domains along with all the sub domains to check
 def checkHTTP(domains, subdomains=[], only_show_http=True):
     url_list = []
+    error_list = []
 
     for domain in domains:
         for subdomain in subdomains:
@@ -11,6 +16,8 @@ def checkHTTP(domains, subdomains=[], only_show_http=True):
                 current_url['source_domain'] = subdomain + '.' + domain
             else:
                 current_url['source_domain'] = domain
+
+            print(current_url['source_domain'])
             
             try:
                 response = requests.get('http://' + current_url['source_domain'], timeout=5)
@@ -27,22 +34,40 @@ def checkHTTP(domains, subdomains=[], only_show_http=True):
             # Check if we only want to save HTTP urls and Errors
             if only_show_http:
                 url_parts = list(filter(None,current_url['final_url'].split('/')))
-                if (url_parts[0] == 'http:' and url_parts[1] == current_url['source_domain']) or current_url['final_url'] == "Error":
+                if url_parts[0] == 'http:' and url_parts[1] == current_url['source_domain'] :
                     url_list.append(current_url)
 
             else:
                 url_list.append(current_url)
+            
+            if current_url['final_url'] == "Error":
+                error_list.append(current_url)
     
     # Print the results
-    print()
+    print("\nDomains Accessible by HTTP: \n")
     for url in url_list:
-        print(url)
-        print()
+        print('{:<35}   -->  {:<25} \n'.format(url['source_domain'], url['final_url']))
+
+    print("Domains That gave errors: \n")
+    for url in error_list:
+        print('{:<35}   -->  {:<25} \n'.format(url['source_domain'], url['final_url']))
     
         
         
-
+# Default Domain List
 domain_list = ["cmls.ca","abovecondos.com",'thewebley.com']
-subdomain_list = ['','www',]
 
+# Get text file name from arguments and read in domains
+if len( sys.argv ) > 1:
+    file = open(sys.argv[1], "r")
+    domain_list = []
+    for line in file:
+        domain_list.append(line.strip())
+
+# Subdomain list to check
+# subdomain_list = ['','www',]
+subdomain_list = ['www',]
+
+# Check the domains
+print("Checking Domains:")
 checkHTTP(domain_list, subdomains=subdomain_list, only_show_http=True)
